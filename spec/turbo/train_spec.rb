da76a702aaec55793fea62aa6d380791add49a5e
goodsign <bond.dmitry@gmail.com>
2022-12-06 16:53:58 +0400

    bundle gem

---
+++ spec/turbo/train_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Turbo::Train do
+  it "has a version number" do
+    expect(Turbo::Train::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
