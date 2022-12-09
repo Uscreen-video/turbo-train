2ec3c3d7799397420e1600af66a84256eb113f55
goodsign <bond.dmitry@gmail.com>
2022-12-09 12:32:24 +0400

    use parallel env var in tests

--- test/dummy/app/controllers/messages_controller.rb
+++ test/dummy/app/controllers/messages_controller.rb
@@ -1,6 +1,5 @@
 class MessagesController < ApplicationController
   def index
     @messages = Message.all
-    @test_messages_id = ENV["TEST_MSG_ID"] || 'messages-local'
   end
 end
--- test/integration/broadcasting_test.rb
+++ test/integration/broadcasting_test.rb
@@ -8,42 +8,44 @@ class BroadcastingTest < ApplicationSystemTestCase
     message_1 = Message.create! user: user_john, text: "Hey Bob!"
     visit messages_path
 
+    list_target = ENV["TEST_MSG_ID"]
+
     assert_no_javascript_errors do
       assert_text "Hey Bob!"
       sleep(2)
 
-      Turbo::Train.broadcast_append_to('messages', target: 'messages', partial: 'messages/message', locals: { message: Message.create!(user: user_bob, text: "Hi John!") })
+      Turbo::Train.broadcast_append_to('messages', target: list_target, partial: 'messages/message', locals: { message: Message.create!(user: user_bob, text: "Hi John!") })
       sleep(2)
       assert_text "Hi John!"
 
-      Turbo::Train.broadcast_prepend_to('messages', target: 'messages', html: '<p>Random7</p>')
+      Turbo::Train.broadcast_prepend_to('messages', target: list_target, html: '<p>Random7</p>')
       sleep(2)
       assert_text "Random7"
 
-      Turbo::Train.broadcast_after_to('messages', target: 'messages', html: '<span>broadcast_after_to</span>')
+      Turbo::Train.broadcast_after_to('messages', target: list_target, html: '<span>broadcast_after_to</span>')
       sleep(2)
       assert_text "broadcast_after_to"
 
-      Turbo::Train.broadcast_before_to('messages', target: 'messages', html: '<span>broadcast_before_to</span>')
+      Turbo::Train.broadcast_before_to('messages', target: list_target, html: '<span>broadcast_before_to</span>')
       sleep(2)
       assert_text "broadcast_before_to"
 
       Message.last.update!(text: "Hi John! How are you? [edited]")
-      Turbo::Train.broadcast_update_to('messages', target: Message.last.id, partial: 'messages/message', locals: { message: Message.last })
+      Turbo::Train.broadcast_update_to('messages', target: ENV["TEST_MSG_ID"] + Message.last.id.to_s, partial: 'messages/message', locals: { message: Message.last })
       sleep(2)
       assert_text "Hi John! How are you? [edited]"
 
       Message.last.update!(text: "Hi John! How's it going? [edited]")
-      Turbo::Train.broadcast_replace_to('messages', target: Message.last.id, partial: 'messages/message', locals: { message: Message.last })
+      Turbo::Train.broadcast_replace_to('messages', target: ENV["TEST_MSG_ID"] + Message.last.id.to_s, partial: 'messages/message', locals: { message: Message.last })
       sleep(2)
       assert_text "Hi John! How's it going? [edited]"
 
       Message.last.update!(text: "Hi John! How's it going? [edited]")
-      Turbo::Train.broadcast_remove_to('messages', target: Message.last.id)
+      Turbo::Train.broadcast_remove_to('messages', target: ENV["TEST_MSG_ID"] + Message.last.id.to_s)
       sleep(2)
       assert_no_text "Hi John"
 
-      Turbo::Train.broadcast_action_to('messages', action: :append, target: 'messages', html: "<p id='message'>2345</p>")
+      Turbo::Train.broadcast_action_to('messages', action: :append, target: list_target, html: "<p id='message'>2345</p>")
       sleep(2)
       assert_text "2345"
     end
