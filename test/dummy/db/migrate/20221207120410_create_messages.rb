d66db7c7f59ac74921e6da89a7ca6425d5afee44
goodsign <bond.dmitry@gmail.com>
2022-12-09 12:19:14 +0400

    specify dynamic rails version for migrations

--- test/dummy/db/migrate/20221207120410_create_messages.rb
+++ test/dummy/db/migrate/20221207120410_create_messages.rb
@@ -1,4 +1,4 @@
-class CreateMessages < ActiveRecord::Migration
+class CreateMessages < ActiveRecord::Migration[ENV['RAILS_VERSION']]
   def change
     create_table :messages do |t|
       t.string :text
