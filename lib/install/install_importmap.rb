e05ed7c180d1c922699d3de485812917cbfa6423
goodsign <bond.dmitry@gmail.com>
2022-12-12 17:47:13 +0400

    move to uscreentv org

--- lib/install/install_importmap.rb
+++ lib/install/install_importmap.rb
@@ -1,5 +1,5 @@
 say "Import Turbo"
-append_to_file "app/javascript/application.js", %(import "@Uscreen-video/turbo-train"\n)
+append_to_file "app/javascript/application.js", %(import "@uscreentv/turbo-train"\n)
 
 say "Pin Turbo"
-append_to_file "config/importmap.rb", %(pin "@Uscreen-video/turbo-train", to: "turbo-train.min.js", preload: true\n)
+append_to_file "config/importmap.rb", %(pin "@uscreentv/turbo-train", to: "turbo-train.min.js", preload: true\n)
