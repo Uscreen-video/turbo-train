0e1989d0e4c8fe75be0962e3fdae02c387271adf
Vladislav Kuznetsov <prettyflackojoy@gmail.com>
2023-08-24 17:27:27 +0300

    chore: try without changes in initializer

--- lib/install/create_initializer.rb
+++ lib/install/create_initializer.rb
@@ -2,20 +2,10 @@ say "Creating initializer"
 create_file Rails.root.join("config/initializers/turbo_train.rb") do
   %{
 Turbo::Train.configure do |config|
+  config.mercure_domain = 'localhost'
+  config.publisher_key = 'testing'
+  config.subscriber_key = 'test'
   config.skip_ssl_verification = true # Development only; don't do this in production
-  config.default_server = :mercure # Default value is :mercure
-
-  config.server :mercure do |mercure|
-    mercure.mercure_domain = 'localhost'
-    mercure.publisher_key = 'testing'
-    mercure.subscriber_key = 'test'
-  end
-
-  config.server :fanout do |fanout|
-    fanout.service_url = ''
-    fanout.service_id = ''
-    fanout.fastly_key = ''
-  end
 end
   }
 end
